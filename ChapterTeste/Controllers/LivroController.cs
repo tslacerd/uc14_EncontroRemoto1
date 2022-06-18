243a0e37244b68a0b82e2f09f567177a584d0507
tslacerd <tslacerd@gmail.com>
2022-06-18 20:45:03 -0300

    First commit

---
+++ ChapterTeste/Controllers/LivroController.cs
@@ -0,0 +1,32 @@
+﻿using ChapterTeste.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChapterTeste.Controllers
+{
+    [Produces("Aplication/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        private readonly LivroRepository _livroRepository;
+        public LivroController(LivroRepository livroRepository)
+        {
+            _livroRepository = livroRepository;
+        }
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_livroRepository.Listar());
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+        }
+    }
+}
---
+++ ChapterTeste/Repositories/LivroRepository.cs
@@ -0,0 +1,22 @@
+﻿using ChapterTeste.Contexts;
+using ChapterTeste.Models;
+
+namespace ChapterTeste.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly ChapterContext _context;
+
+        public LivroRepository(ChapterContext context)
+        {
+
+            _context = context;
+
+        }
+
+        public List<Livro> Listar()
+        {
+            return _context.Livros.ToList();
+        }
+    }
+}
